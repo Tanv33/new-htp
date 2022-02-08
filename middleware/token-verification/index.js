12f298bb27cb5b5de656127ee0ee7b5929c4cb48
Tanv33 <tanveer.khan2692000@gmail.com>
2022-02-08 15:04:01 +0500

    check console on heroku

--- middleware/token-verification/index.js
+++ middleware/token-verification/index.js
@@ -3,26 +3,31 @@ const { SECRET } = require("../../config");
 const { findOne } = require("../../helpers");
 
 const tokenVerification = (req, res, next) => {
-    let token = req.headers["token"];
-    if (!token) {
-        return res.status(404).send({ status: 404, message: "No token provided!" });
+  let token = req.headers["token"];
+  if (!token) {
+    return res.status(404).send({ status: 404, message: "No token provided!" });
+  }
+  jwt.verify(token, SECRET, async (err, decoded) => {
+    if (err) {
+      console.log(err);
+      return res
+        .status(400)
+        .send({ status: 400, message: "Token Unauthorized!" });
     }
-    jwt.verify(token, SECRET, async(err, decoded) => {
-        if (err) {
-            console.log(err);
-            return res
-                .status(400)
-                .send({ status: 400, message: "Token Unauthorized!" });
-        }
-        const isUserExist = await findOne("user", { _id: decoded.user._id });
-        if (!isUserExist) {
-            return res
-                .status(404)
-                .send({ status: 404, message: "User does not exist." });
-        }
-        req.userId = decoded.user._id;
-        next();
-    });
+    if (!decoded.user._id) {
+      return res
+        .status(401)
+        .send({ status: 400, message: "Kaha na kai user ka new token generate karoo" });
+    }
+    const isUserExist = await findOne("user", { _id: decoded.user._id });
+    if (!isUserExist) {
+      return res
+        .status(404)
+        .send({ status: 404, message: "User does not exist." });
+    }
+    req.userId = decoded.user._id;
+    next();
+  });
 };
 
 module.exports = { tokenVerification: tokenVerification };
