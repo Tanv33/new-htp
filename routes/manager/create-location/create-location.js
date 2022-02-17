b79845dbf23e079c3bc3811e90febe38bcc22764
Tanv33 <tanveer.khan2692000@gmail.com>
2022-02-17 17:20:41 +0500

    location updated

--- routes/manager/create-location/create-location.js
+++ routes/manager/create-location/create-location.js
@@ -9,7 +9,7 @@ const fs = require("fs");
 
 let testItems = Joi.object().keys({
   name: Joi.string().required(),
-  types: Joi.string().required(),
+  types: Joi.required(),
 });
 
 let patientItems = Joi.object().keys({
@@ -18,51 +18,51 @@ let patientItems = Joi.object().keys({
 });
 
 const schema = Joi.object({
-  // location_name: Joi.string().required(),
-  // email: Joi.string().email().required(),
-  // zip_code: Joi.string().required(),
-  // address: Joi.string().required(),
-  // city: Joi.string().required(),
-  // // state: Joi.string().required(),
-  // location_logo: Joi.string(),
-  test: Joi.array().items(testItems),
-  patient_required_fields: Joi.array().items(patientItems),
+  location_name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  zip_code: Joi.string().required(),
+  address: Joi.string().required(),
+  city: Joi.string().required(),
+  // state: Joi.string().required(),
+  location_logo: Joi.string(),
+  test: Joi.array().items(testItems).required(),
+  patient_required_fields: Joi.array().items(patientItems).required(),
 });
 
 const createLocation = async (req, res) => {
   try {
-    // await schema.validateAsync(req.body);
-    res.send(req.body);
+    await schema.validateAsync(req.body);
+    // res.send(req.body);
 
-    // if (!req.body.location_logo) {
-    //   if (!req.file) {
-    //     return res
-    //       .status(400)
-    //       .send({ status: 400, message: "Location Logo Required" });
-    //   }
-    // }
-    // if (req.file) {
-    //   req.body.location_logo = await getDropBoxLink(
-    //     "/location logo/" +
-    //       generateRandomNumber(1111, 9999) +
-    //       "-" +
-    //       req.file.filename,
-    //     req.file.path
-    //   );
-    //   await fs.unlinkSync(req.file.path);
-    // }
+    if (!req.body.location_logo) {
+      if (!req.file) {
+        return res
+          .status(400)
+          .send({ status: 400, message: "Location Logo Required" });
+      }
+    }
+    if (req.file) {
+      req.body.location_logo = await getDropBoxLink(
+        "/location logo/" +
+          generateRandomNumber(1111, 9999) +
+          "-" +
+          req.file.filename,
+        req.file.path
+      );
+      await fs.unlinkSync(req.file.path);
+    }
 
-    // const locationCreated = await insertNewDocument("location", req.body);
-    // const { _id } = locationCreated;
-    // console.log({ locationCreated });
-    // await pushIfNotExists(
-    //   "user",
-    //   { _id: req.userId },
-    //   { manager_location: _id }
-    // );
-    // return res
-    //   .status(200)
-    //   .send({ status: 200, message: "Location Created Successfully" });
+    const locationCreated = await insertNewDocument("location", req.body);
+    const { _id } = locationCreated;
+    console.log({ locationCreated });
+    await pushIfNotExists(
+      "user",
+      { _id: req.userId },
+      { manager_location: _id }
+    );
+    return res
+      .status(200)
+      .send({ status: 200, message: "Location Created Successfully" });
   } catch (e) {
     console.log(e);
     return res.status(400).send({ status: 400, message: e.message });
