56eda7b64cbdab4c3e9dcedcf1b0927dfaebdd42
Tanv33 <tanveer.khan2692000@gmail.com>
2022-02-17 19:15:26 +0500

    joi

--- routes/patient/add-patient.js
+++ routes/patient/add-patient.js
@@ -55,18 +55,18 @@ const addPatient = async (req, res) => {
     // }
     // obj.patient_signature = Joi.required();
     // console.log(obj);
-    if (gender === "Female") {
-      if (!pregnant) {
+    if (req.body.gender === "Female") {
+      if (!req.body.pregnant) {
         obj["pregnant"] = Joi.required();
         return res
           .status(400)
           .send({ status: 400, message: "Pregnant field required" });
       }
     }
-    if (payment === "Insurance") {
+    if (req.body.payment === "Insurance") {
       if (
-        !insurance_name &&
-        !insurance_policy_number &&
+        !req.body.insurance_name &&
+        !req.body.insurance_policy_number &&
         !req.files.insurance_image[0]
       ) {
         obj["insurance_name"] = Joi.required();
@@ -78,8 +78,8 @@ const addPatient = async (req, res) => {
         });
       }
     }
-    if (us_id === "Yes") {
-      if (!us_id_no) {
+    if (req.body.us_id === "Yes") {
+      if (!req.body.us_id_no) {
         obj["us_id_no"] = Joi.required();
         return res.status(400).send({
           status: 400,
@@ -88,8 +88,8 @@ const addPatient = async (req, res) => {
       }
     }
 
-    if (us_id === "No") {
-      if (!ssn) {
+    if (req.body.us_id === "No") {
+      if (!req.body.ssn) {
         obj["ssn"] = Joi.required();
         return res.status(400).send({
           status: 400,
