0a71e3d5fdd6cdba16f55d28f0004f290c9e1e33
Tanv33 <tanveer.khan2692000@gmail.com>
2022-02-19 16:21:25 +0500

    rough updated

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,7 @@ const test_Type = require("./test-type");
 const admin = require("./admin");
 const patient = require("./patient");
 const manager = require("./manager");
+const { upload } = require("../lib");
 
 // AUTH Routes * /api/auth/*
 router.use("/auth", auth);
@@ -20,7 +21,8 @@ router.use("/test", tokenVerification, test_Type);
 router.use("/patient", tokenVerification, mpVerification, patient);
 router.use("/admin", tokenVerification, adminVerification, admin);
 router.use("/manager", tokenVerification, managerVerification, manager);
-router.post("/rough", (req, res) => {
+router.post("/rough", upload.single("logo"), (req, res) => {
+  console.log(req.body);
   return res.status(200).send(req.body);
 });
 
