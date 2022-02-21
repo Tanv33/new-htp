c3a5178e5a90185aad412abd54426162fd1ebbe2
Tanv33 <tanveer.khan2692000@gmail.com>
2022-02-21 14:58:32 +0500

    cors origin updated 2

--- routes/auth/verification-code/index.js
+++ routes/auth/verification-code/index.js
@@ -1,4 +1,4 @@
-const Joi = require("Joi");
+const Joi = require("joi");
 const { findOneSort, findOne } = require("../../../helpers");
 const bcrypt = require("bcryptjs");
 const { SECRET } = require("../../../config");
