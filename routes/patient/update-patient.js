f0c25dec2b66955f307e728e6430632e1ea18c3d
Tanv33 <tanveer.khan2692000@gmail.com>
2022-02-24 15:39:43 +0500

    patient login

--- routes/patient/update-patient.js
+++ routes/patient/update-patient.js
@@ -1,42 +1,42 @@
 const { updateDocument } = require("../../helpers");
-const Joi = require("joi");
+// const Joi = require("joi");
 
-const schema = Joi.object({
-  first_name: Joi.string().required(),
-  last_name: Joi.string().required(),
-  full_name: Joi.string().required(),
-  email: Joi.string().email().required(),
-  delivery_method: Joi.array().required(),
-  is_tested: Joi.string(),
-  is_insured: Joi.boolean(),
-  telephone: Joi.string().min(14).required(),
-  address: Joi.string().required(),
-  city: Joi.string().required(),
-  state: Joi.string().required(),
-  zip_code: Joi.string().required(),
-  date_of_birth: Joi.date().required(),
-  age: Joi.number().required(),
-  created_by: Joi.string().required(), //user Id Here
-  location: Joi.string(),
-  signature: Joi.string().required(), // PDF file Here
-  doc_prescription: Joi.boolean(),
-  doc_prescription_url: Joi.string().required(),
-  test_type: Joi.string().required(),
-  uuid: Joi.number().required(),
-  sex: Joi.string().required(),
-  passport: Joi.boolean(),
-  covid_test_form: Joi.string().required(), // PDF file
-  id_image: Joi.string().required(),
-  identity_card: Joi.string().required(),
-  insurance_image: Joi.string().required(),
-  patient_test_result_sign_off: Joi.string().required(), // PDF file
-  tested_by: Joi.string().required(), // Object id of who tested
-  tested_date: Joi.date().required(),
-});
+// const schema = Joi.object({
+//   first_name: Joi.string().required(),
+//   last_name: Joi.string().required(),
+//   gender: Joi.string().required(),
+//   email: Joi.string().email().required(),
+//   delivery_method: Joi.array().required(),
+//   is_tested: Joi.string(),
+//   is_insured: Joi.boolean(),
+//   telephone: Joi.string().min(14).required(),
+//   address: Joi.string().required(),
+//   city: Joi.string().required(),
+//   state: Joi.string().required(),
+//   zip_code: Joi.string().required(),
+//   date_of_birth: Joi.date().required(),
+//   age: Joi.number().required(),
+//   created_by: Joi.string().required(), //user Id Here
+//   location: Joi.string(),
+//   signature: Joi.string().required(), // PDF file Here
+//   doc_prescription: Joi.boolean(),
+//   doc_prescription_url: Joi.string().required(),
+//   test_type: Joi.string().required(),
+//   uuid: Joi.number().required(),
+//   sex: Joi.string().required(),
+//   passport: Joi.boolean(),
+//   covid_test_form: Joi.string().required(), // PDF file
+//   id_image: Joi.string().required(),
+//   identity_card: Joi.string().required(),
+//   insurance_image: Joi.string().required(),
+//   patient_test_result_sign_off: Joi.string().required(), // PDF file
+//   tested_by: Joi.string().required(), // Object id of who tested
+//   tested_date: Joi.date().required(),
+// });
 
 const updatePatient = async (req, res) => {
   try {
-    await schema.validateAsync(req.body);
+    // await schema.validateAsync(req.body);
     const test_type_updated = await updateDocument(
       "patient",
       { _id: req.params.id },
---
+++ routes/public/patient-login.js
@@ -0,0 +1,31 @@
+const { findOne } = require("../../helpers");
+const Joi = require("joi");
+
+const patientLoginSchema = Joi.object({
+  first_name: Joi.string().required(),
+  last_name: Joi.string().required(),
+  date_of_birth: Joi.string().required(),
+  pid: Joi.string().required(),
+});
+
+const patientLogin = async (req, res) => {
+  try {
+    await patientLoginSchema.validateAsync(req.query);
+    const { first_name, last_name, date_of_birth, pid } = req.query;
+    const patient = await findOne("patient", {
+      first_name,
+      last_name,
+      date_of_birth,
+      pid,
+    });
+    if (!patient) {
+      return res.status(404).send({ status: 404, message: "No Patient Found" });
+    }
+    return res.status(200).send({ status: 200, patient });
+  } catch (e) {
+    console.log(e);
+    return res.status(400).send({ status: 400, message: e.message });
+  }
+};
+
+module.exports = patientLogin;
