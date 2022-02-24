e3f12ad9af11c484b415442fc51667a8426b9a15
Tanv33 <tanveer.khan2692000@gmail.com>
2022-02-24 17:22:03 +0500

    test type update api completed

--- routes/test-type/update-test-type.js
+++ routes/test-type/update-test-type.js
@@ -1,30 +1,49 @@
-const { updateDocument } = require("../../helpers");
+const {
+  updateDocument,
+  pushIfNotExists,
+  findOneAndUpdate,
+} = require("../../helpers");
 const Joi = require("joi");
 
 const schema = Joi.object({
   name: Joi.string(),
-  types: Joi.array(),
+  deleteType: Joi.array(),
+  addType: Joi.array(),
 });
 
 const updateTestType = async (req, res) => {
   try {
     await schema.validateAsync(req.body);
-    const { types, name } = req.body;
+    const { deleteType, addType, name } = req.body;
     if (name) {
-      const updated = await updateDocument(
-        "testType",
-        { _id: req.params.id },
-        { name }
-      );
-      return res.status(200).send({ status: 200, updated });
+      await updateDocument("testType", { _id: req.params.id }, { name });
+      return res
+        .status(200)
+        .send({ status: 200, message: "Name updated successfully" });
     }
-    if (types) {
-      const test_type_updated = await updateDocument(
+    if (deleteType) {
+      await findOneAndUpdate(
         "testType",
         { _id: req.params.id },
-        { types }
+        {
+          $pullAll: {
+            types: deleteType,
+          },
+        }
       );
-      return res.status(200).send({ status: 200, test_type_updated });
+      return res
+        .status(200)
+        .send({ status: 200, message: "Type deleted successfully" });
+    }
+    if (addType) {
+      await pushIfNotExists(
+        "testType",
+        { _id: req.params.id },
+        { types: addType }
+      );
+      return res
+        .status(200)
+        .send({ status: 200, message: "Type added successfully" });
     }
   } catch (e) {
     res.status(400).send({ status: 400, message: e.message });
