1e9102d017117b226a074fa92f65d4ded3c05f51
Tanv33 <tanveer.khan2692000@gmail.com>
2022-03-03 15:49:08 +0500

    manager location  patient updated and delete api created

--- routes/manager/patient/index.js
+++ routes/manager/patient/index.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const deleteLocationPatient = require("./delete-location-patient");
 const router = express.Router();
 const getPatientForm = require("./get-form");
 const getLocationPatient = require("./get-location-patient");
 const searchPatient = require("./search-patient");
+const updateLocationPatient = require("./update-location-patient");
 
 router.get("/get-form", getPatientForm);
 router.get("/get-location-patient", getLocationPatient);
 router.get("/search-patient", searchPatient);
+router.post("/update-location-patient/:id", updateLocationPatient);
+router.delete("/delete-location-patient", deleteLocationPatient);
 
 module.exports = router;
