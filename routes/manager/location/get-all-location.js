fac37cba1d114a896d0ee057d00ad1cbf097e98f
Tanv33 <tanveer.khan2692000@gmail.com>
2022-03-12 15:22:11 +0500

    get all location api updated

--- routes/manager/location/get-all-location.js
+++ routes/manager/location/get-all-location.js
@@ -17,17 +17,17 @@ const getAllLocation = async (req, res) => {
       let patients = await getCount("patient", {
         location_id: manager_location[index]._id,
       });
-      let employees = await getCount("user", {
-        employee_location: manager_location[index]._id,
-      });
-      let no_of_tested = await getCount("patient", {
-        location_id: manager_location[index]._id,
-        is_tested: "Yes",
-      });
-      let no_of_not_tested = await getCount("patient", {
-        location_id: manager_location[index]._id,
-        is_tested: "No",
-      });
+      // let employees = await getCount("user", {
+      //   employee_location: manager_location[index]._id,
+      // });
+      // let no_of_tested = await getCount("patient", {
+      //   location_id: manager_location[index]._id,
+      //   is_tested: "Yes",
+      // });
+      // let no_of_not_tested = await getCount("patient", {
+      //   location_id: manager_location[index]._id,
+      //   is_tested: "No",
+      // });
       let no_of_medical_profession = await getCount("user", {
         employee_location: manager_location[index]._id,
         type: medicalProfession._id,
@@ -36,24 +36,24 @@ const getAllLocation = async (req, res) => {
         employee_location: manager_location[index]._id,
         type: labTechnician._id,
       });
-      let no_of_covid_patient = await getCount("patient", {
-        location_id: manager_location[index]._id,
-        "test_type.name": "Covid",
-      });
-      let no_of_omicron_patient = await getCount("patient", {
-        location_id: manager_location[index]._id,
-        "test_type.name": "Omicron",
-      });
+      // let no_of_covid_patient = await getCount("patient", {
+      //   location_id: manager_location[index]._id,
+      //   "test_type.name": "Covid",
+      // });
+      // let no_of_omicron_patient = await getCount("patient", {
+      //   location_id: manager_location[index]._id,
+      //   "test_type.name": "Omicron",
+      // });
       // console.log(no_of_omicron_patient);
       // A single function for how many number of Covid and Omicron patient are there in the patient colloection
       manager_location[index].noOfPatient = patients;
-      manager_location[index].noOfEmployees = employees;
-      manager_location[index].noOfTested = no_of_tested;
-      manager_location[index].noOfNotTested = no_of_not_tested;
+      // manager_location[index].noOfEmployees = employees;
+      // manager_location[index].noOfTested = no_of_tested;
+      // manager_location[index].noOfNotTested = no_of_not_tested;
       manager_location[index].noOfMedicalProfession = no_of_medical_profession;
       manager_location[index].noOfLabTechnician = no_of_lab_technician;
-      manager_location[index].noOfCovidPatient = no_of_covid_patient;
-      manager_location[index].noOfOmicronPatient = no_of_omicron_patient;
+      // manager_location[index].noOfCovidPatient = no_of_covid_patient;
+      // manager_location[index].noOfOmicronPatient = no_of_omicron_patient;
     }
     return res
       .status(200)
