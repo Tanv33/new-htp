908a55a629ba9af14f000e16ac3edf552aa9232e
Tanv33 <tanveer.khan2692000@gmail.com>
2022-03-15 18:15:58 +0500

    employee chart modifyed

--- routes/manager/chart/employee-chart.js
+++ routes/manager/chart/employee-chart.js
@@ -1,8 +1,13 @@
-const { findOne, getCount } = require("../../../helpers");
+const { findOne, getCount, findOneAndPopulate } = require("../../../helpers");
 
 const employeeChart = async (req, res) => {
   try {
-    const manager = await findOne("user", { _id: req.userId });
+    const manager = await findOneAndPopulate(
+      "user",
+      { _id: req.userId },
+      "manager_location",
+      "location_name"
+    );
     const medicalProfession = await findOne("userType", {
       type: "Medical Profession",
     });
@@ -13,20 +18,23 @@ const employeeChart = async (req, res) => {
     const { manager_location } = manager;
     for (let i = 0; i < manager_location.length; i++) {
       const noOfLabTechnician = await getCount("user", {
-        employee_location: manager_location[i],
+        employee_location: manager_location[i]?._id,
         type: labTechnicican._id,
       });
       const noOfMedicalProfession = await getCount("user", {
-        employee_location: manager_location[i],
+        employee_location: manager_location[i]?._id,
         type: medicalProfession._id,
       });
       let obj = {};
-      obj.location = manager_location[i];
+      obj.location_id = manager_location[i]?._id;
+      obj.location_name = manager_location[i]?.location_name;
       obj.medicalProfession = noOfMedicalProfession;
       obj.labTechnicican = noOfLabTechnician;
       employeeChartArr.push(obj);
     }
-    return res.status(200).send({ status: 200, employeeChartArr });
+    return res
+      .status(200)
+      .send({ status: 200, length: employeeChartArr.length, employeeChartArr });
   } catch (e) {
     console.log(e);
     return res.status(400).send({ status: 400, message: e.message });
