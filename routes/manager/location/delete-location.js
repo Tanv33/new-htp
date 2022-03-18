c309ea1a2fa40e9a59a4823eaf7179ed1ad77eb1
Tanv33 <tanveer.khan2692000@gmail.com>
2022-03-18 15:22:44 +0500

    admin can edit and delete locaiton

--- routes/manager/location/delete-location.js
+++ routes/manager/location/delete-location.js
@@ -1,4 +1,9 @@
-const { deleteDocument, findOneAndUpdate } = require("../../../helpers");
+const {
+  deleteDocument,
+  findOneAndUpdate,
+  findOneAndPopulate,
+  findOne,
+} = require("../../../helpers");
 const Joi = require("joi");
 
 const schema = Joi.object({
@@ -9,15 +14,41 @@ const deleteLocation = async (req, res) => {
   try {
     await schema.validateAsync(req.body);
     const { _id } = req.body;
-    await findOneAndUpdate(
+    const checkLocaitonExist = await findOne("location", { _id });
+    if (!checkLocaitonExist) {
+      return res
+        .status(400)
+        .send({ status: 400, message: "No location found with your given id" });
+    }
+    const checkUser = await findOneAndPopulate(
       "user",
-      { _id: req.userId },
       {
-        $pullAll: {
-          manager_location: [_id],
-        },
-      }
+        _id: req.userId,
+      },
+      "type"
     );
+    if (checkUser?.type?.type === "Manager") {
+      await findOneAndUpdate(
+        "user",
+        { _id: req.userId },
+        {
+          $pullAll: {
+            manager_location: [_id],
+          },
+        }
+      );
+    }
+    if (checkUser?.type?.type === "Asins") {
+      await findOneAndUpdate(
+        "user",
+        { _id: checkLocaitonExist?.created_by },
+        {
+          $pullAll: {
+            manager_location: [_id],
+          },
+        }
+      );
+    }
     await deleteDocument("location", { _id });
     return res
       .status(200)
@@ -28,3 +59,96 @@ const deleteLocation = async (req, res) => {
   }
 };
 module.exports = deleteLocation;
+
+// const {
+//   deleteDocument,
+//   findOneAndUpdate,
+//   findOneAndPopulate,
+//   findOne,
+// } = require("../../../helpers");
+// const Joi = require("joi");
+
+// const managerSchema = Joi.object({
+//   _id: Joi.string().required(),
+// });
+
+// const adminSchema = Joi.object({
+//   _id: Joi.string().required(),
+//   manager_id: Joi.string().required(),
+// });
+
+// const deleteLocation = async (req, res) => {
+//   try {
+//     const { manager_id, _id } = req.body;
+//     const checkUser = await findOneAndPopulate(
+//       "user",
+//       {
+//         _id: req.userId,
+//       },
+//       "type"
+//     );
+//     if (checkUser?.type?.type === "Manager") {
+//       await managerSchema.validateAsync(req.body);
+//     }
+//     if (checkUser?.type?.type === "Asins") {
+//       await adminSchema.validateAsync(req.body);
+//       const findManager = await findOne("user", {
+//         _id: manager_id,
+//       });
+//       // console.log(findManager);
+//       if (!findManager) {
+//         return res.status(404).send({
+//           status: 404,
+//           message: "NO manager found with your given manager id",
+//         });
+//       }
+//       const checkAtuhorOfLocation = await findOne("user", {
+//         _id: manager_id,
+//         manager_location: _id,
+//       });
+//       console.log(checkAtuhorOfLocation);
+//       if (!checkAtuhorOfLocation) {
+//         return res.status(400).send({
+//           status: 400,
+//           message: "This location does not belong  to your given manager",
+//         });
+//       }
+//     }
+//     const checkLocaitonExist = await findOne("location", { _id });
+//     if (!checkLocaitonExist) {
+//       return res
+//         .status(400)
+//         .send({ status: 400, message: "No location found with your given id" });
+//     }
+//     if (checkUser?.type?.type === "Manager") {
+//       await findOneAndUpdate(
+//         "user",
+//         { _id: req.userId },
+//         {
+//           $pullAll: {
+//             manager_location: [_id],
+//           },
+//         }
+//       );
+//     }
+//     if (checkUser?.type?.type === "Asins") {
+//       await findOneAndUpdate(
+//         "user",
+//         { _id: manager_id },
+//         {
+//           $pullAll: {
+//             manager_location: [_id],
+//           },
+//         }
+//       );
+//     }
+//     await deleteDocument("location", { _id });
+//     return res
+//       .status(200)
+//       .send({ status: 200, message: "Location deleted successfully" });
+//   } catch (e) {
+//     console.log(e);
+//     return res.status(400).send({ status: 400, message: e.message });
+//   }
+// };
+// module.exports = deleteLocation;
--- routes/manager/location/update-location.js
+++ routes/manager/location/update-location.js
@@ -1,4 +1,4 @@
-const { updateDocument } = require("../../../helpers");
+const { updateDocument, findOne } = require("../../../helpers");
 const Joi = require("joi");
 
 let testItems = Joi.object().keys({
@@ -28,6 +28,15 @@ const schema = Joi.object({
 const updateLocation = async (req, res) => {
   try {
     await schema.validateAsync(req.body);
+    const checkLocaitonExist = await findOne("location", {
+      _id: req.params.id,
+    });
+    if (!checkLocaitonExist) {
+      return res
+        .status(400)
+        .send({ status: 400, message: "No location found with your given id" });
+    }
+    console.log(checkLocaitonExist);
     const updatedLocation = await updateDocument(
       "location",
       { _id: req.params.id },
@@ -45,3 +54,90 @@ const updateLocation = async (req, res) => {
 };
 
 module.exports = updateLocation;
+
+// const { updateDocument } = require("../../../helpers");
+// const Joi = require("joi");
+
+// let testItems = Joi.object().keys({
+//   name: Joi.string(),
+//   types: Joi.array(),
+// });
+
+// let patientItems = Joi.object().keys({
+//   field: Joi.string(),
+//   required: Joi.string(),
+// });
+
+// const managerSchema = Joi.object({
+//   location_name: Joi.string(),
+//   email: Joi.string().email(),
+//   zip_code: Joi.string(),
+//   consent: Joi.string(),
+//   address: Joi.string(),
+//   city: Joi.string(),
+//   business_or_individual: Joi.string(),
+//   send_copy_to_email: Joi.boolean(),
+//   location_logo: Joi.string(),
+//   test: Joi.array().items(testItems),
+//   patient_required_fields: Joi.array().items(patientItems),
+// });
+
+// const adminSchema = Joi.object({
+//   location_name: Joi.string(),
+//   email: Joi.string().email(),
+//   zip_code: Joi.string(),
+//   consent: Joi.string(),
+//   address: Joi.string(),
+//   city: Joi.string(),
+//   business_or_individual: Joi.string(),
+//   send_copy_to_email: Joi.boolean(),
+//   location_logo: Joi.string(),
+//   test: Joi.array().items(testItems),
+//   patient_required_fields: Joi.array().items(patientItems),
+//   manager_id: Joi.string().required(),
+// });
+
+// const updateLocation = async (req, res) => {
+//   try {
+//     const { manager_id } = req.body;
+//     const checkUser = await findOneAndPopulate(
+//       "user",
+//       {
+//         _id: req.userId,
+//       },
+//       "type"
+//     );
+//     if (checkUser?.type?.type === "Manager") {
+//       await managerSchema.validateAsync(req.body);
+//     }
+//     if (checkUser?.type?.type === "Asins") {
+//       await adminSchema.validateAsync(req.body);
+//       const findManager = await findOne("user", {
+//         _id: manager_id,
+//       });
+//       console.log(findManager);
+//       if (!findManager) {
+//         return res.status(404).send({
+//           status: 404,
+//           message: "NO manager found with your given manager id",
+//         });
+//       }
+//     }
+
+//     const updatedLocation = await updateDocument(
+//       "location",
+//       { _id: req.params.id },
+//       req.body
+//     );
+//     return res.status(200).send({
+//       status: 200,
+//       message: "Location Updated Successfully",
+//       updatedLocation,
+//     });
+//   } catch (e) {
+//     console.log(e);
+//     return res.status(400).send({ status: 400, message: e.message });
+//   }
+// };
+
+// module.exports = updateLocation;
