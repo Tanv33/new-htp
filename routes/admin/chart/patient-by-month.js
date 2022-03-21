3d43029c1e61ac6be1f9b84a017d7ee2acc32e16
Tanv33 <tanveer.khan2692000@gmail.com>
2022-03-21 19:15:19 +0500

    admin api for patient by month

---
+++ routes/admin/chart/patient-by-month.js
@@ -0,0 +1,58 @@
+const { getAggregate, findOne } = require("../../../helpers");
+const patientByMonthsChart = async (req, res) => {
+  try {
+    const months = [
+      "",
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    const patientArr = await getAggregate("patient", [
+      {
+        $match: {},
+      },
+      {
+        $group: {
+          _id: {
+            monthNumber: { $month: "$createdAt" },
+            // year: { $year: "$createdAt" },
+            year: { $year: new Date() },
+          },
+          totalPatient: {
+            $sum: 1,
+          },
+          testedPatient: {
+            $sum: {
+              $cond: [{ $eq: ["$is_tested", "Yes"] }, 1, 0],
+            },
+          },
+          unTestedPatient: {
+            $sum: {
+              $cond: [{ $eq: ["$is_tested", "No"] }, 1, 0],
+            },
+          },
+        },
+      },
+      {
+        $sort: { _id: 1 },
+      },
+    ]);
+    for (let i = 0; i < patientArr.length; i++) {
+      patientArr[i].month = months[patientArr[i]._id.monthNumber];
+    }
+    res.status(200).send({ status: 200, patientArr });
+  } catch (e) {
+    console.log(e);
+    return res.status(400).send({ status: 400, message: e.message });
+  }
+};
+module.exports = patientByMonthsChart;
