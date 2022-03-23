735243fa24e674aa7348ab96d2702b64c85d01a4
Tanv33 <tanveer.khan2692000@gmail.com>
2022-03-23 15:24:27 +0500

    date api for employee created

---
+++ routes/employee/chart/date.js
@@ -0,0 +1,58 @@
+const Joi = require("joi");
+const { getAggregate } = require("../../../helpers");
+const { ObjectID } = require("../../../types");
+
+const schema = Joi.object({
+  from: Joi.date().required(),
+  to: Joi.date().required(),
+});
+
+const dateChart = async (req, res) => {
+  try {
+    await schema.validateAsync(req.body);
+    const { from, to } = req.body;
+    // Graph 1
+    const graph1 = await getAggregate("patient", [
+      {
+        $match: {
+          created_by: ObjectID(req.userId),
+          createdAt: { $gte: new Date(from), $lte: new Date(to) },
+        },
+      },
+      {
+        $group: {
+          _id: "$test_type.name",
+          noOfPatient: {
+            $sum: 1,
+          },
+        },
+      },
+    ]);
+
+    // Graph 2
+    const graph2 = await getAggregate("patient", [
+      {
+        $match: {
+          created_by: ObjectID(req.userId),
+          createdAt: { $gte: new Date(from), $lte: new Date(to) },
+        },
+      },
+      {
+        $group: {
+          _id: { type: "$test_type.type", name: "$test_type.name" },
+          noOfPatient: {
+            $sum: 1,
+          },
+        },
+      },
+      {
+        $sort: { "_id.name": 1 },
+      },
+    ]);
+    return res.status(200).send({ status: 200, graph1, graph2 });
+  } catch (e) {
+    console.log(e);
+    return res.status(400).send({ status: 400, message: e.message });
+  }
+};
+module.exports = dateChart;
--- routes/employee/chart/index.js
+++ routes/employee/chart/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const basicGraph = require("./basic");
+const dateChart = require("./date");
 const patientByMonth = require("./patient-by-month");
 
 router.get("/basic", basicGraph);
 router.get("/patient-by-month", patientByMonth);
+router.get("/date", dateChart);
 module.exports = router;
