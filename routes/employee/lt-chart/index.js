808f8119be40509cd7fcc2841ef9f8f29c373295
Tanv33 <tanveer.khan2692000@gmail.com>
2022-03-25 15:50:33 +0500

    patient by month  finalize

--- routes/employee/lt-chart/index.js
+++ routes/employee/lt-chart/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const basicChart = require("./basic");
 const dateChart = require("./date");
+const patientByMonthsChart = require("./patient-by-month");
 const router = express.Router();
 
 router.get("/basic", basicChart);
 router.get("/date", dateChart);
+router.get("/patient-by-month", patientByMonthsChart);
 
 module.exports = router;
---
+++ routes/employee/lt-chart/patient-by-month.js
@@ -0,0 +1,62 @@
+const { getAggregate, findOne } = require("../../../helpers");
+
+const patientByMonthsChart = async (req, res) => {
+  try {
+    const months = [
+      "",
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    const user = await findOne("user", { _id: req.userId });
+    const { employee_location } = user;
+
+    const patientArr = await getAggregate("patient", [
+      {
+        $match: { location_id: employee_location },
+      },
+      {
+        $group: {
+          _id: {
+            monthNumber: { $month: "$createdAt" },
+            // year: { $year: "$createdAt" },
+            year: { $year: new Date() },
+          },
+          totalPatient: {
+            $sum: 1,
+          },
+          testedPatient: {
+            $sum: {
+              $cond: [{ $eq: ["$is_tested", "Yes"] }, 1, 0],
+            },
+          },
+          unTestedPatient: {
+            $sum: {
+              $cond: [{ $eq: ["$is_tested", "No"] }, 1, 0],
+            },
+          },
+        },
+      },
+      {
+        $sort: { _id: 1 },
+      },
+    ]);
+    for (let i = 0; i < patientArr.length; i++) {
+      patientArr[i].month = months[patientArr[i]._id.monthNumber];
+    }
+    res.status(200).send({ status: 200, patientArr });
+  } catch (e) {
+    console.log(e);
+    return res.status(400).send({ status: 400, message: e.message });
+  }
+};
+module.exports = patientByMonthsChart;
