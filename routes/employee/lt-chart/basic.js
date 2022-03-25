df9fdf60234e6f29b5cd5e2d8ab41afa5128d5bf
Tanv33 <tanveer.khan2692000@gmail.com>
2022-03-25 08:39:07 +0500

    graph2

--- routes/employee/lt-chart/basic.js
+++ routes/employee/lt-chart/basic.js
@@ -39,31 +39,63 @@ const basicChart = async (req, res) => {
     ]);
 
     // Graph 2
-    // const graph2 = await getAggregate("patient", [
-    //   {
-    //     $match: {
-    //       location_id: employee_location,
-    //       "test_type.type": { $ne: "Rapid" },
-    //     },
-    //   },
-    //   {
-    //     $group: {
-    //       _id: "$is_tested",
-    //       noOfPatient: {
-    //         $sum: 1,
-    //       },
-    //     },
-    //   },
-    //   {
-    //     $addFields: { is_tested: "$_id" },
-    //   },
-    //   {
-    //     $project: { _id: 0 },
-    //   },
-    // ]);
+    const graph2 = await getAggregate("patient", [
+      {
+        $match: {
+          location_id: employee_location,
+          "test_type.type": { $ne: "Rapid" },
+          is_tested: "Yes",
+        },
+      },
+      {
+        $group: {
+          _id: "$created_by",
+          negative: {
+            $sum: {
+              $cond: [{ $eq: ["$patient_result", "Negative"] }, 1, 0],
+            },
+          },
+          positive: {
+            $sum: {
+              $cond: [{ $eq: ["$patient_result", "Positive"] }, 1, 0],
+            },
+          },
+          inconclusive: {
+            $sum: {
+              $cond: [{ $eq: ["$patient_result", "Inconclusive"] }, 1, 0],
+            },
+          },
+          pending: {
+            $sum: {
+              $cond: [{ $eq: ["$patient_result", "Pending"] }, 1, 0],
+            },
+          },
+        },
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "_id",
+          foreignField: "_id",
+          as: "employee",
+        },
+      },
+      // {
+      //   $project: {
+      //     "employee.first_name": 1,
+      //     "employee.last_name": 1,
+      //   },
+      // },
+      // {
+      //   $addFields: { is_tested: "$_id" },
+      // },
+      // {
+      //   $project: { _id: 0 },
+      // },
+    ]);
 
-    // return res.status(200).send({ status: 200, graph1, graph2 });
-    return res.status(200).send({ status: 200, graph1 });
+    return res.status(200).send({ status: 200, graph1, graph2 });
+    // return res.status(200).send({ status: 200, graph1 });
   } catch (e) {
     console.log(e);
     return res.status(400).send({ status: 400, message: e.message });
