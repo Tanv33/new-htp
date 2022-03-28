9199d0c4d2a1c1f7afc691a691128357d5f63483
Tanv33 <tanveer.khan2692000@gmail.com>
2022-03-29 02:50:47 +0500

    get put and delete product api created 1

--- routes/admin/manager/get-managers-name.js
+++ routes/admin/manager/get-managers-name.js
@@ -4,7 +4,11 @@ const getManagersName = async (req, res) => {
   try {
     const manager = await findOne("userType", { type: "Manager" });
     const { _id } = manager;
-    const managers = await findAndSelect("user", { type: _id }, "full_name");
+    const managers = await findAndSelect(
+      "user",
+      { type: _id },
+      "full_name mid"
+    );
     return res
       .status(200)
       .send({ status: 200, length: managers.length, managers });
