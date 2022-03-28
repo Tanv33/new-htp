360b1150795538ae8ba57debf74d774c45e72252
Tanv33 <tanveer.khan2692000@gmail.com>
2022-03-29 02:58:26 +0500

    get maager pagination added

--- routes/admin/approve-manager/get-managers.js
+++ routes/admin/approve-manager/get-managers.js
@@ -1,15 +1,30 @@
-const { findOne, find, getPopulatedData } = require("../../../helpers");
+const Joi = require("joi");
+const {
+  findOne,
+  find,
+  getPopulatedData,
+  getPopulatedDataWithLimit,
+} = require("../../../helpers");
 
+const schema = Joi.object({
+  page: Joi.string().required(),
+});
 const getManagers = async (req, res) => {
   try {
+    await schema.validateAsync(req.query);
+    const { page } = req.query;
     const findManagerId = await findOne("userType", { type: "Manager" });
     const { _id } = findManagerId;
-    const managersArray = await getPopulatedData(
+    const managersArray = await getPopulatedDataWithLimit(
       "user",
       {
         type: _id,
       },
-      "type manager_location"
+      "type manager_location",
+      "",
+      { _id: -1 },
+      page,
+      6
     );
     return res.status(200).send({ status: 200, managersArray });
   } catch (e) {
